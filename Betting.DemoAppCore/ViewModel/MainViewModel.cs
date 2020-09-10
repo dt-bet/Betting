9a49c7efcb8d447591af5afb937a78bfc4f1d8b7
dtaylor-530 <d.taylor530@gmail.com>
2020-09-10 20:10:02 +0100

    Cleans

--- Betting.DemoAppCore/ViewModel/MainViewModel.cs
+++ Betting.DemoAppCore/ViewModel/MainViewModel.cs
@@ -10,31 +10,29 @@ namespace Betting.DemoWpfApp
 {
     class MainViewModel
     {
-        IEnumerable<DataRow> points { get; }
-        IEnumerator<DataRow> pointsEnumerator { get; }
+        readonly IEnumerable<DataRow> points;
+        IEnumerator<DataRow> pointsEnumerator;
 
         public MainViewModel()
         {
-
-            //DataSource = startdata;
             var csv = File.ReadAllText("../../../Data/E0H.csv");
-            //Points = new List<Tuple<double, double>>();
             DateTime dtn = DateTime.Now;
-            points = Csv.CsvReader.ReadFromText(csv).Select(line =>
-            {
-                DateTime dt = dtn + TimeSpan.FromDays(int.Parse(line["index"]));
-                double prc = double.Parse(line["B365H"]);
-                double prd = double.Parse(line["B365D"]);
-                bool rslt = double.Parse(line["UPH"]) > 0;
-                return new DataRow { date = dt, price = prc, prediction = prd, result = rslt };
-            });
+            points = Csv.CsvReader.ReadFromText(csv)
+                .Select(line =>
+                {
+                    DateTime dt = dtn + TimeSpan.FromDays(int.Parse(line["index"]));
+                    double prc = double.Parse(line["B365H"]);
+                    double prd = double.Parse(line["B365D"]);
+                    bool rslt = double.Parse(line["UPH"]) > 0;
+                    return new DataRow { date = dt, price = prc, prediction = prd, result = rslt };
+                });
 
             pointsEnumerator = points.GetEnumerator();
 
             Observable.Interval(TimeSpan.FromSeconds(1)).Subscribe(_ =>
             {
                 if (pointsEnumerator.MoveNext())
-                Application.Current.Dispatcher.Invoke(()=> Points.Add(pointsEnumerator .Current));
+                    Application.Current.Dispatcher.Invoke(() => Points.Add(pointsEnumerator.Current));
             });
 
         }
@@ -42,10 +40,5 @@ namespace Betting.DemoWpfApp
 
         public ICollection<DataRow> Points { get; } = new ObservableCollection<DataRow>();
 
-        public IEnumerable<DataRow> Points2 =>points;
-
-
-
-     
     }
 }
